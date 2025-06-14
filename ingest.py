483cbb4c91282bef0c70027eca1acb1229ff2ebb
Hakeem Oluwatobi <Hakeemsalaudeen01@gmail.com>
2025-06-14 19:02:21 +0100

    kestra-iceberg-demo v4

--- ingest.py
+++ ingest.py
@@ -19,16 +19,6 @@ TABLE = "raw_fruits"
 S3_PATH = f"s3://{BUCKET_NAME}/{TABLE}"
 S3_PATH_TMP = f"{S3_PATH}_tmp"
 
-# MERGE_QUERY = """
-# MERGE INTO fruits f USING raw_fruits r
-#     ON f.fruit = r.fruit
-#     WHEN MATCHED
-#         THEN UPDATE
-#             SET id = r.id, berry = r.berry, update_timestamp = current_timestamp
-#     WHEN NOT MATCHED
-#         THEN INSERT (id, fruit, berry, update_timestamp)
-#               VALUES(r.id, r.fruit, r.berry, current_timestamp);
-# """
 
 MERGE_QUERY = """
 MERGE INTO fruits f USING (
